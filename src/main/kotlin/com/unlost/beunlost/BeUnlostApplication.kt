cb37d66b3057bd7deb49cfce591402430791f168
Phuong Dong Cu <phuong.dong.cu@gmail.com>
2023-02-26 22:35:56 +0100

    init project

---
+++ src/main/kotlin/com/unlost/beunlost/BeUnlostApplication.kt
@@ -0,0 +1,11 @@
+package com.unlost.beunlost
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeUnlostApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeUnlostApplication>(*args)
+}
---
+++ src/test/kotlin/com/unlost/beunlost/BeUnlostApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unlost.beunlost
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeUnlostApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
